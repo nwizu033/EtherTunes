dd3641280abb655ec6f71b497dee61ad8a90b3a2
chinwizu <chinwianthony@gmail.com>
2023-04-23 23:40:00 +0100

    God abeg

--- client/src/pages/whitepaper/index.jsx
+++ client/src/pages/whitepaper/index.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import styles from '../../styles/documentation.module.css';
 
 const whitepaper = () => {
   return (
-    <div>whitepaper</div>
+    <div>
+      <div className={styles.docs}>
+          <a href='https://app.gitbook.com/o/J7nKfvAEzUPYMvNtA9SZ/s/brRSxa7KCLGS9NIyw8lx/~/changes/3/ethertunes-token/deployed-contracts'>Click to see documentation</a>
+      </div>
+      
+
+    </div>
   )
 }
 
